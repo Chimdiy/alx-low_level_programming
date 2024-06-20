b8286e295b8ddab73e9577a9a3c8f34706d44ffa
Chimdiy <rayprincess3@gmail.com>
2024-06-20 13:41:07 -0700

    qoute commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<unistd.h>
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
+/*
+ *main - function main is the start point of the program
+ *
+ *Description : the main function displaying a message without using a funtion
+ *
+ *Return : result is 1 to show error
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+        char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+        write(2, m, 59);
+        return (1);
 }
