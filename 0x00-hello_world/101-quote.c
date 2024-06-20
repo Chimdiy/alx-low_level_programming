c30fe9d27ebd4893b076fe937078f9c05f8df677
Chimdiy <rayprincess3@gmail.com>
2024-06-20 14:02:12 -0700

    qoute commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
  */
 int main(void)
 {
-        char m[]= "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-        write(2,m,59);
-        return(1);
+	write(2, m, 59);
+	return (1);
 }
