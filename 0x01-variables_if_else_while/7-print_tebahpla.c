d57912735d1275b684f8edcba44a99b584417a72
Chimdiy <rayprincess3@gmail.com>
2024-06-21 04:27:24 -0700

    Tebeh commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -9,13 +8,14 @@
  */
 int main(void)
 {
-    char c = 'z';
+	char c = 'z';
 
-    while (c >= 'a') {
-        putchar(c);
-        c--;
-    }
-    putchar('\n');
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
