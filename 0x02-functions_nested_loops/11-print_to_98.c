b57ee5eedfc934342c7231a617b1f8697e41e218
Chimdiy <rayprincess3@gmail.com>
2024-06-25 13:53:32 -0700

    Eleventh commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,13 +5,7 @@
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-	    putchar('-');
-        n = -n;
-    }
-
-    if (n / 10)
+    if ((n < 0)) || ((n / 10))
     {
         print_number(n / 10);
     }
