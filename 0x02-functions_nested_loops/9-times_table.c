a0e0408541158ced389c2a8c3241bf4c3d47765f
Chimdiy <rayprincess3@gmail.com>
2024-06-25 14:05:06 -0700

    Table commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * print_times_table - prints the 9 times table, starting with 0
+ */
+void print_times_table(void)
+{
+    int i, j;
+    int product;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            product = i * j;
+            
+            if (j != 0)
+            {
+                putchar(',');
+                putchar(' ');
+                if (product < 10)
+                {
+                    putchar(' ');
+                }
+            }
+            
+            if (product < 10)
+            {
+                putchar(product + '0');
+            }
+            else
+            {
+                putchar((product / 10) + '0');
+                putchar((product % 10) + '0');
+            }
+        }
+        putchar('\n');
+    }
+}
+
