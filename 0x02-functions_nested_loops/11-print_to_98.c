fb7ae1da1d0c5ce344b01b2a150649a816a49f99
Chimdiy <rayprincess3@gmail.com>
2024-06-25 14:02:40 -0700

    Eleventh commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,36 +5,16 @@
  */
 void print_number(int n)
 {
-    if ((n < 0)) || ((n / 10))
+    if (n < 0)
+    {
+        putchar('-');
+        n = -n;
+    }
+
+    if (n / 10)
     {
         print_number(n / 10);
     }
 
     putchar((n % 10) + '0');
-}
-
-/**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: the starting number
- */
-void print_to_98(int n)
-{
-    while (n != 98)
-    {
-        print_number(n);
-        putchar(',');
-        putchar(' ');
-
-        if (n < 98)
-        {
-            n++;
-        }
-        else
-        {
-            n--;
-        }
-    }
-
-    print_number(98);
-    putchar('\n');
 }
