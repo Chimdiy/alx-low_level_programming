72fcf8c8382ee280f272b4637506e8fa1b992c90
Chimdiy <rayprincess3@gmail.com>
2024-06-25 12:28:09 -0700

    Third commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * checks for lower case character
+ *
+ * return 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+int main (void);
+{
+	int _islower(int c);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
