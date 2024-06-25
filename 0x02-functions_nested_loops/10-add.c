d5e95f1e543aeea5b8bab7904969c9a64dee6f5e
Chimdiy <rayprincess3@gmail.com>
2024-06-25 13:58:53 -0700

    tenth commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -11,24 +11,3 @@ int add(int a, int b)
 {
     return (a + b);
 }
-
-/**
- * main - tests the add function
- *
- * Return: 0 on success
- */
-int main(void)
-{
-    int result;
-
-    result = add(2, 3);
-    printf("2 + 3 = %d\n", result); /* Should print 5 */
-
-    result = add(-1, 5);
-    printf("-1 + 5 = %d\n", result); /* Should print 4 */
-
-    result = add(0, 0);
-    printf("0 + 0 = %d\n", result); /* Should print 0 */
-
-    return (0);
-}
